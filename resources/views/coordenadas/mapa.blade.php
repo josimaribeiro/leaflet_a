2ebb8323ad11edf051897a585102195d980016e0
profjosimar <josimar.ribeiro@eteparacambi.com.br>
2025-09-12 10:35:53 -0300

    Update at 12/09/2025 10:35:53,73

--- resources/views/coordenadas/mapa.blade.php
+++ resources/views/coordenadas/mapa.blade.php
@@ -21,9 +21,9 @@
 
     <div id="map"></div>
 
-    <!-- Dados das coordenadas em JSON vindos do controller -->
+    <!-- Passagem das coordenadas em JSON para o JS -->
     <script>
-        const coordenadas = {!! $coordenadasJson !!}
+        const coordenadas = {!! $coordenadasJson !!};
     </script>
 
     <!-- Leaflet JS -->
@@ -36,34 +36,42 @@
                 return;
             }
 
+            // Cria o mapa na div #map
             const map = L.map('map');
 
-// Cria um array com todas as coordenadas
-const bounds = L.latLngBounds([]);
-
-coordenadas.forEach(function (coord) {
-    const marker = L.marker([coord.latitude, coord.longitude])
-        .addTo(map)
-        .bindPopup(`<strong>${coord.nome}</strong><br>Lat: ${coord.latitude}<br>Lng: ${coord.longitude}`);
-
-    // Adiciona a coordenada ao bounds
-    bounds.extend(marker.getLatLng());
-});
-
-// Ajusta o mapa para mostrar todos os marcadores
-map.fitBounds(bounds);
-
-
+            // Adiciona os tiles do Geoapify
             L.tileLayer('https://maps.geoapify.com/v1/tile/osm-liberty/{z}/{x}/{y}.png?apiKey={{ env('GEOAPIFY_API_KEY') }}', {
                 maxZoom: 18,
                 attribution: '&copy; <a href="https://www.geoapify.com/">Geoapify</a> contributors'
             }).addTo(map);
 
-            coordenadas.forEach(function (coord) {
-                L.marker([coord.latitude, coord.longitude])
-                    .addTo(map)
-                    .bindPopup(`<strong>${coord.nome}</strong><br>Lat: ${coord.latitude}<br>Lng: ${coord.longitude}`);
+            // Cria bounds para ajustar o zoom
+            const bounds = L.latLngBounds([]);
+
+            // Define o ícone personalizado de árvore
+            const treeIcon = L.icon({
+                iconUrl: 'https://cdn-icons-png.flaticon.com/128/740/740934.png',
+                iconSize: [32, 32],      // largura e altura do ícone em px
+                iconAnchor: [16, 32],    // ponto do ícone que ficará na coordenada
+                popupAnchor: [0, -32]    // ponto do ícone onde o popup abrirá
             });
+
+            // Para cada coordenada, adiciona um marcador com o ícone e popup
+            coordenadas.forEach(function (coord) {
+                const marker = L.marker([coord.latitude, coord.longitude], { icon: treeIcon })
+                    .addTo(map)
+                    .bindPopup(
+                        `<strong>${coord.nome}</strong><br>` +
+                        `Lat: ${coord.latitude}<br>` +
+                        `Lng: ${coord.longitude}`
+                    );
+
+                // Expande os limites do mapa para incluir esse marcador
+                bounds.extend(marker.getLatLng());
+            });
+
+            // Ajusta o zoom e a área para mostrar todos os marcadores
+            map.fitBounds(bounds);
         });
     </script>
 </body>
